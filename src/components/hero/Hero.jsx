d4459c1cf757e8e8951a44288c062deedd77102c
mito01100010 <dimitar.pashkulev@gmail.com>
2024-05-16 15:35:13 +0300

    Add Hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Hero.scss'
+import Header from './../header/Header'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <Header />
+
+      <figure className='hero-image'>
+        <img src='./hero.jpg' alt='' />
+      </figure>
+
+      <div className='hero-inner'>
+        <div className='shell'>
+          <div className='hero-text'>
+            <h1>Hello there,</h1>
+
+            <h2>
+              My name is Dimitar
+              I'm into Web Development
+            </h2>
+
+            <a href='#about'>About me</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
